d96c549709dd007af6aef4140500abd37f946900
shoppingzh <xpzheng6@163.com>
2022-12-02 23:36:36 +0800

    feat: 0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
 import axios from 'axios'
+import { createWriteStream, mkdirSync, PathLike, readdirSync, rmSync } from 'fs'
+import AdmZip from 'adm-zip'
+import { extname, resolve } from 'path'
+import fs from 'fs-extra'
 
-(async function() {
-  // TODO
-})()
+const TEMP = resolve(__dirname, '../.tmp')
+
+rmSync(TEMP, { force: true, recursive: true })
+mkdirSync(TEMP)
+
+interface Options {
+  pid: string,
+  token: string,
+  destDir: string,
+}
+
+async function pipStream(data: any, path: PathLike) {
+  return new Promise((resolve, reject) => {
+    const stream = createWriteStream(path)
+    stream.on('error', reject)
+    stream.on('finish', resolve)
+    data.pipe(stream)
+  })
+}
+
+function isUsefulFile(filename: string) {
+  return ['.css', '.woff', '.woff2', '.svg', '.ttf'].indexOf(extname(filename)) >= 0
+    && /^iconfont/.test(filename)
+}
+
+export async function download(options: Options) {
+  const { data } = await axios({
+    url: 'https://www.iconfont.cn/api/project/download.zip',
+    params: {
+      pid: options.pid,
+    },
+    responseType: 'stream',
+    headers: {
+      cookie: `EGG_SESS_ICONFONT=${options.token}`
+    }
+  })
+  const zipFile = resolve(TEMP, './download.zip')
+  await pipStream(data, zipFile)
+
+  const az = new AdmZip(zipFile)
+  az.extractAllTo(TEMP, true)
+  const dirs = readdirSync(TEMP, { withFileTypes: true }).filter(o => o.isDirectory() && /^font_/.test(o.name))
+  if (!dirs.length) return
+  const dir = resolve(TEMP, dirs[0].name)
+  readdirSync(dir, { withFileTypes: true })
+    .filter(o => o.isFile() && isUsefulFile(o.name))
+    .forEach(o => {
+      fs.copyFileSync(resolve(dir, o.name), resolve(options.destDir, o.name))
+    })
+  // 删掉tmp目录
+  rmSync(TEMP, { force: true, recursive: true })
+}
+
